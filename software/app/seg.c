138eb5b8200d5caa89f579848a7a1cc523f49295
Krahmy <alexisfournier41@gmail.com>
2024-01-15 17:19:27 +0100

    supp alt_printf 7seg

--- software/app/seg.c
+++ software/app/seg.c
@@ -17,13 +17,13 @@ volatile __uint8_t cnt1 = 0,cnt2 = 0, cnt3 = 0;
 // Function to display 7-segment representation
 static void irqhandler(void* context, alt_u32 id)
 {
-    alt_printf("yo\n");
+    
 
     IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE, cnt1);
     IOWR_ALTERA_AVALON_PIO_DATA(PIO_2_BASE, cnt2);
     IOWR_ALTERA_AVALON_PIO_DATA(PIO_3_BASE, cnt3);
     cnt1++;
-    usleep(10000);
+   
     if (cnt1 == 10) {
         cnt1 = 0;
         cnt2++;
@@ -43,7 +43,7 @@ static void irqhandler(void* context, alt_u32 id)
 
 int main()
 {
-    alt_printf("main() started\n");
+   
 
     
     
